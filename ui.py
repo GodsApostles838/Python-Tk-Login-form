514d5139d3e15aa965ea944097dd53e630475f09
GodsApostles838 <139572136+GodsApostles838@users.noreply.github.com>
2023-07-27 17:12:01 +1000

    Update ui.py

--- ui.py
+++ ui.py
@@ -28,11 +28,11 @@ class LoginPage(tk.Tk):
         self.message_label = tk.Label(frame_login, text="", bg="black", fg="white", font=("Arial", 12))
         self.message_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-        self.entry_username.bind("<Enter>", self.on_enter_username)  # Bind hover event for username entry
-        self.entry_username.bind("<Leave>", self.on_leave_username)  # Bind leave event for username entry
+        self.entry_username.bind("<Enter>", self.on_enter_username) 
+        self.entry_username.bind("<Leave>", self.on_leave_username)  
 
-        self.entry_password.bind("<Enter>", self.on_enter_password)  # Bind hover event for password entry
-        self.entry_password.bind("<Leave>", self.on_leave_password)  # Bind leave event for password entry
+        self.entry_password.bind("<Enter>", self.on_enter_password) 
+        self.entry_password.bind("<Leave>", self.on_leave_password) 
 
         self.geometry("300x200")
         self.resizable(False, False)
@@ -41,7 +41,7 @@ class LoginPage(tk.Tk):
         username = self.entry_username.get()
         password = self.entry_password.get()
 
-        # Perform login authentication logic here
+        
         if username == "admin" and password == "password":
             self.message_label.config(text="Login successful!", fg="green")
             system_username = getpass.getuser()
@@ -72,7 +72,7 @@ class LoginPage2(tk.Toplevel):
         self.configure(bg="white")
         self.geometry("1500x500")
 
-        self.clock_in_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Record the clock-in time
+        self.clock_in_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
 
         label_message = tk.Label(self, text=f"Welcome, {system_username}!", bg="white", fg="black",
                                  font=("Arial", 16, "bold"))
@@ -81,31 +81,31 @@ class LoginPage2(tk.Toplevel):
         self.label_time = tk.Label(self, text="", bg="white", fg="black", font=("Arial", 12))
         self.label_time.pack(pady=10)
 
-        self.update_time()  # Update the time label initially
-        self.after(1000, self.update_time)  # Schedule the periodic update of time
+        self.update_time()  
+        self.after(1000, self.update_time)  
 
-        # Frame for buttons
+        
         button_frame = tk.Frame(self, bg="white")
         button_frame.pack(pady=10)
 
-        # Button to save data
+       
         button_save = tk.Button(button_frame, text="Save Data", width=15, height=2, font=("Arial", 12),
                                 command=self.save_data)
         button_save.pack(side=tk.LEFT, padx=(0, 10))
 
-        # Button to view saved data
+        
         button_view_data = tk.Button(button_frame, text="View Saved Data", width=15, height=2, font=("Arial", 12),
                                      command=self.view_data)
         button_view_data.pack(side=tk.LEFT)
 
-        # Additional buttons
+        
         button1 = tk.Button(button_frame, text="Button 1", width=15, height=2, font=("Arial", 12))
         button1.pack(side=tk.LEFT, padx=(10, 0))
 
         button2 = tk.Button(button_frame, text="Button 2", width=15, height=2, font=("Arial", 12))
         button2.pack(side=tk.LEFT)
 
-        # Fields for name, age, and email
+       
         field_frame = tk.Frame(self, bg="white")
         field_frame.pack(pady=20)
 
@@ -127,14 +127,14 @@ class LoginPage2(tk.Toplevel):
         self.entry_email = tk.Entry(field_frame, bg="white", fg="black", font=("Arial", 12))
         self.entry_email.pack(side=tk.LEFT)
 
-        # Clock in label
+        
         clock_in_label = tk.Label(self, text=f"User {system_username} clocked in at: {self.clock_in_time}",
                                   bg="white", fg="black", font=("Arial", 12))
         clock_in_label.pack(side=tk.BOTTOM, pady=10)
 
-        self.saved_data = []  # List to store the saved data
+        self.saved_data = []  
 
-        # Add validation functions to enforce input restrictions
+        
         validate_age = self.register(self.validate_age)
         self.entry_age.config(validate="key", validatecommand=(validate_age, "%S"))
 
@@ -143,12 +143,11 @@ class LoginPage2(tk.Toplevel):
 
 
     def update_time(self):
-        current_time = datetime.now().strftime("%H:%M:%S")  # Get the current time
+        current_time = datetime.now().strftime("%H:%M:%S")  
         self.label_time.config(text="Current Time: " + current_time)
-        self.after(1000, self.update_time)  # Schedule the next update after 1 second
+        self.after(1000, self.update_time)  
 
     def validate_age(self, char):
-        # Allow only numeric characters and backspace
         if char.isdigit() or char == "\b":
             return True
         return False
@@ -158,40 +157,39 @@ class LoginPage2(tk.Toplevel):
         age = self.entry_age.get()
         email = self.entry_email.get()
 
-        # Check if any of the fields are blank
+       
         if name.strip() == "" or age.strip() == "" or "@" not in email:
             messagebox.showerror("Error", "All fields must be filled and a valid email address must be provided.")
             return False
 
-        # Store the saved data as a dictionary
+       
         data_entry = {"Name": name, "Age": age, "Email": email}
         self.saved_data.append(data_entry)
 
-        # Create a custom dialog window
+       
         dialog = tk.Toplevel()
         dialog.title("Data Saved")
         dialog.geometry("400x200")
 
-        # Set the font size for the message label
+        
         message_font = ("Arial", 14)
 
-        # Create a label to display the message
+       
         message = f"Data saved:\nName: {name}\nAge: {age}\nEmail: {email}"
         message_label = tk.Label(dialog, text=message, font=message_font)
         message_label.pack(padx=20, pady=10)
 
-        # Add a button to close the dialog
+       
         close_button = tk.Button(dialog, text="Close", command=dialog.destroy, width=10)
         close_button.pack(pady=10)
 
     def view_data(self):
         if self.saved_data:
-            # Create a new window for displaying data and search functionality
             view_data_window = tk.Toplevel()
             view_data_window.title("View Saved Data")
             view_data_window.geometry("600x400")
 
-            # Create a search bar
+           
             search_frame = tk.Frame(view_data_window, bg="white")
             search_frame.pack(pady=10)
 
@@ -205,36 +203,34 @@ class LoginPage2(tk.Toplevel):
                                       command=lambda: self.search_data(search_entry.get()))
             search_button.pack(side=tk.LEFT)
 
-            # Create a data display area
+            
             data_frame = tk.Frame(view_data_window, bg="white")
             data_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
             self.data_text = tk.Text(data_frame, bg="white", fg="black", font=("Arial", 12))
             self.data_text.pack(fill=tk.BOTH, expand=True)
 
-            # Display the saved data in the data text widget
+            
             for data_entry in self.saved_data:
                 self.data_text.insert(tk.END, f"Name: {data_entry['Name']}\nAge: {data_entry['Age']}\nEmail: {data_entry['Email']}\n\n")
 
-            # Add a button to close the window
+           
             close_button = tk.Button(view_data_window, text="Close", command=view_data_window.destroy,
                                       width=10)
             close_button.pack(pady=10)
         else:
-            # Show a message if no data has been saved
             messagebox.showinfo("No Data", "No data has been saved.")
 
     def search_data(self, search_text):
-        # Clear the current data display
         self.data_text.delete(1.0, tk.END)
 
-        # Search for matching data entries
+
         matching_entries = []
         for data_entry in self.saved_data:
             if search_text.lower() in data_entry["Name"].lower():
                 matching_entries.append(data_entry)
 
-        # Display the matching data entries
+       
         for data_entry in matching_entries:
             self.data_text.insert(tk.END, f"Name: {data_entry['Name']}\nAge: {data_entry['Age']}\nEmail: {data_entry['Email']}\n\n")
 
